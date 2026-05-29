707283ecd126d030610c121f28ed05e6cd6d66b4
Santiago Sanchez <santiagosanchezramirez809@gmail.com>
2026-05-28 23:24:00 -0500

    feat: implementar subclase GalloCampero con herencia gallo y resistir()

---
+++ src/GalloCampero.java
@@ -0,0 +1,30 @@
+public class GalloCampero extends Gallo{
+
+    private String procedencia;
+    public GalloCampero(String nombre, double peso,String procedencia){
+        super(nombre, peso,120,20);
+        this.procedencia=procedencia;
+    }
+    public String getProcedencia(){
+        return procedencia;
+    }
+    public void setProcedencia(String p){
+        this.procedencia=p;
+    }
+    @Override
+    public String pelear(){
+        return getNombre()+" (campero) Le mete el pico sin miedo!"+" Golpe duro de "+fuerza+" de fuerza.";
+    }
+    @Override
+    public String cacarear(){
+        return getNombre()+" desde "+procedencia+": KIKIRIKIIIII rudo y fuerte! ";
+
+    }
+    public String resistir(){
+        int nueva=getEnergia()+ 15;
+        setEnergia(nueva);
+        return getNombre()+" aguanta y se repone! Energia: "+getEnergia();
+    }
+
+
+}
