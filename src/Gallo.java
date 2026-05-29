daab004364f766bab144eb9565cf14ea2eaca4ba
Santiago Sanchez <santiagosanchezramirez809@gmail.com>
2026-05-28 23:09:52 -0500

    feat: agregar metodos getters setters y comportimiento a Gallo

--- src/Gallo.java
+++ src/Gallo.java
@@ -15,7 +15,63 @@ public class Gallo {
         this.victorias=0;
         this.estaActivo=true;
 
-
+    }
+    public String getNombre(){
+        return nombre;
 
     }
+    public double getPeso(){
+        return peso;
+    }
+
+    public int getEnergia() {
+        return energia;
+    }
+    public int getFuerza(){
+        return fuerza;
+
+    }
+
+    public int getVictorias() {
+        return victorias;
+    }
+
+    public boolean isEstaActivo() {
+        return estaActivo;
+    }
+
+    public void setEnergia(int energia){
+        if(energia < 0)energia=0;
+        this.energia=energia;
+        if(this.energia==0)this.estaActivo=false;
+
+    }
+
+    public void setFuerza(int fuerza){
+        this.fuerza=fuerza;
+    }
+    public void setEstaActivo(boolean v){
+        this.estaActivo=v;
+    }
+    public void registrarVictoria(){
+        this.victorias++;
+    }
+    public String pelear(){
+        return nombre + "realizar un atasque basico!(fuerza: "+fuerza+")";
+
+    }
+    public String cacarear(){
+        return nombre+ ": Co Co Co";
+    }
+    public String verEstado(){
+        String estado= estaActivo ? "En pie" : "Caido";
+        return "["+nombre +" | Energia: "+energia+ " | Victorias: "+victorias+" | "+estado+" ]";
+
+    }
+    @Override
+    public String toString(){
+        return nombre +" (peso: "+peso+" kg | Fuerza: "+fuerza+" | Energia: "+energia+")";
+    }
+
+
 }
